387afbe0e4b232502b89410c4da518ee684d8c69
Dan <dvwhite1981@gmail.com>
2023-08-15 11:15:49 -0400

    Complete

--- public/main.js
+++ public/main.js
@@ -3,33 +3,65 @@
 // For storing user's theme selection in cookies
 function storeTheme(themeName) {
     // Your code here
+    const themeCookie = `theme=${themeName}; max-age=15`;
+    document.cookie = themeCookie;
 }
 
 // For restoring theme from cookies, if selected by the user in the past
 function restoreTheme() {
     // Your code here
+    const theme = getCookieValue("theme");
+    if (theme) {
+        setTheme(theme);
+    }
 }
 
 // For clearing theme selection from cookies (reset to default)
 function clearTheme() {
     // Your code here
+    document.cookie = "theme=; max-age=0";
 }
+
+// Helper function
+const getCookieValue = (target) => {
+    const cookie = document.cookie
+        .split("; ")
+        .find((row) => row.startsWith(target));
+    let value;
+
+    if (cookie) {
+        value = cookie.split("=")[1];
+    } else {
+        value = null;
+    }
+
+    return value;
+}
+
 
 /* ================================ PHASE 3 ================================ */
 
 // For storing user's display name in cookies
 function storeName(displayName) {
     // Your code here
+    const nameCookie = `displayName=${displayName}`;
+    document.cookie = nameCookie;
 }
 
 // For restoring user's display name from cookies, if set in the past
 function restoreName() {
     // Your code here
+    const name = getCookieValue("displayName");
+
+    if (name) {
+        setInputValue("display-name", name);
+    }
 }
 
 // For clearing user's display name from cookies
 function clearName() {
     // Your code here
+    document.cookie = "displayName=; max-age=0";
 }
 
 /* ========================================================================= */
